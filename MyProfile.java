998f5483ddec76ca7e880d0f1643d09054010bd7
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-10 16:35:15 +0300

    feat: variables Review

---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Andres F. Regalado Bucheli";
+    int age = 45;
+    double desiredSalary = 100000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
