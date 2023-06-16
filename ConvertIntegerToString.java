86e0da1fe7dce3d8f89435923e15dd11c783a44c
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-16 11:58:46 +0300

    feat: Casting and Converting -ConvertIntegerToString.java

---
+++ ConvertIntegerToString.java
@@ -0,0 +1,9 @@
+public class ConvertIntegerToString {
+    public static void main(String[] args) {
+        int numInt = 15;
+        System.out.println("Int value: " + numInt);
+        // Convert int value to String
+        String numString = String.valueOf(numInt);
+        System.out.println("String value: " + numString);
+    }
+}
