4fa1d6ce95e0f4e9b0d4fc7ffdffdbd580f415ae
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-16 11:54:07 +0300

    feat: Casting and Converting -WideningTypeCasting.java

---
+++ WideningTypeCasting.java
@@ -0,0 +1,8 @@
+public class WideningTypeCasting {
+    public static void main(String[] args) {
+        int numInt = 9;
+        System.out.println("Int value: " + numInt);
+        // convert an int to a double
+        System.out.println("Double value: " + (double)numInt);
+    }
+}
