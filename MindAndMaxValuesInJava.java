26a44176f39390c20117eb6af7a9a6b8b780bb9c
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-16 11:43:42 +0300

    feat: Casting and Converting -creating documentation and adding NarrowTypeCasting.java, CastingAndRound.java, and MindAndMaxValuesInJava.java

---
+++ MindAndMaxValuesInJava.java
@@ -0,0 +1,6 @@
+public class MindAndMaxValuesInJava {
+    public static void main(String[] args) {
+        System.out.println("Min: " + Integer.MIN_VALUE);
+        System.out.println("Max: " + Integer.MAX_VALUE);
+    }
+}
---
+++ NarrowTypeCasting.java
@@ -0,0 +1,10 @@
+public class NarrowTypeCasting {
+    public static void main(String[] args) { 
+        double numDouble = 12.99;
+        System.out.println("Double value: " + numDouble);
+        // convert a double to int
+        int numInt = (int) numDouble;
+        System.out.println("Int value: " + numInt);
+    }
+}
+
